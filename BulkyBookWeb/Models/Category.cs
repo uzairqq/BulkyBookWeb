b432651c27e3454e9adeb8f30e844410c42d1ae9
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-23 19:48:09 +0500

    Added Data Annotations

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedOn { get; set; } = DateTime.Now;
