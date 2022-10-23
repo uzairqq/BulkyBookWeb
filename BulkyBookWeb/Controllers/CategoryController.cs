bf124a50f71d5f89edb7086726543892195a1d38
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-24 01:12:16 +0500

    Retreive All Categories

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly BulkyBookDbContext _bulkyBookDbContext;
+
+        public CategoryController(BulkyBookDbContext bulkyBookDbContext)
+        {
+            _bulkyBookDbContext = bulkyBookDbContext;
+        }
+
+        
         public IActionResult Index()
         {
-            return View();
+            var categories = _bulkyBookDbContext.Categories.ToList();
+            return View(categories);
         }
     }
 }
