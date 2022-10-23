692b325844dad0fed3a76cedea6999b9eb3d934a
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-23 19:37:53 +0500

    Added a New Model Called Category

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
