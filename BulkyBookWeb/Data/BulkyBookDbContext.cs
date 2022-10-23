cee2db4adf3df8b7b96e0011d6954700dc5e112d
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-23 20:30:25 +0500

    Added DbContext For Database Creating And Installed Entity Framework Core.

---
+++ BulkyBookWeb/Data/BulkyBookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class BulkyBookDbContext : DbContext
+    {
+        public BulkyBookDbContext(DbContextOptions<BulkyBookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
