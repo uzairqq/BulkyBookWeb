6c709c4f56d618c1a3009fa39f2e308bef969646
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-23 20:36:59 +0500

    Using DbContext in Program.cs File With Its Connection String

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,8 +1,12 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<BulkyBookDbContext>(options=>options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +16,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
